e474279cde869d02b281ad3f005985a6fcc5c05a
calin91m <calinmorar1991@yahoo.com>
2025-05-25 22:59:08 +0200

    05 fixing-image-links

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ["https://images.fashion-trading.com"], // Replace with your image domain
+  },
   /* config options here */
 };
 
